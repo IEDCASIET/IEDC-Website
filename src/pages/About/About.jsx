25b4149fc6e57c14118d2a2c4cb888e6ee6fdf5b
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-11-02 19:41:12 +0530

    made changes to about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,14 +3,14 @@ import logo from "../../assets/IEDC ASIET Logo.png";
 
 const About = () => {
   return (
-    <section className="bg-white-500 text-black h-screen flex flex-col justify-center mt-121">
+    <section className="bg-white-500 text-black h-screen flex flex-col justify-center mt-20">
         <div className="text-center mb-4">
             <h1 className="text-7xl font-medium inline">About</h1>&nbsp;&nbsp;
             <h1 className="text-7xl font-semibold inline">Us</h1>
         </div>
         <div className='relative flex items-center'>
-            <img src={logo} alt="Logo" className="h-121 w-auto ml-20 mt=122" />
-             <h2 className='text-black font-semibold text-5xl ml-0 mt-122'>IEDC ASIET</h2>
+            <img src={logo} alt="Logo" className="h-120 w-auto ml-80" />
+             <h2 className='text-black font-semibold text-5xl ml-60 mt-122'>IEDC ASIET</h2>
         </div>
     </section>
   );
