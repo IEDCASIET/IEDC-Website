4f578d4201da4a0a2d9006493f217e32f55e05c9
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-11-04 19:43:50 +0530

    added Events and excom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // import { Element } from 'react-scroll';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Events from './pages/Events/Events';
+import Excom from './pages/Excom/Excom';
 // import Navbar from './components/Navbar/Navbar';
 
 export default function App() {
@@ -9,7 +11,8 @@ export default function App() {
       {/* <Navbar /> */}
       <Home />
       <About />
-      {/* <Events /> */}
+      <Events />
+      <Excom />
     </div>
   );
 }
