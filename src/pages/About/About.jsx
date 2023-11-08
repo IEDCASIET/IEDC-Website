927eeb6d470080ef7da983698c842d112bc8d81f
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-11-09 02:49:56 +0530

    created Events section and about and made changes in about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,14 +3,18 @@ import logo from "../../assets/IEDC ASIET Logo.png";
 
 const About = () => {
   return (
-    <section className="bg-white-500 text-black h-screen flex flex-col justify-center mt-20">
+    <section id="about-us" className="bg-white-500 text-black h-screen flex flex-col justify-center mt-60">
         <div className="text-center mb-4">
             <h1 className="text-7xl font-medium inline">About</h1>&nbsp;&nbsp;
             <h1 className="text-7xl font-semibold inline">Us</h1>
         </div>
         <div className='relative flex items-center'>
             <img src={logo} alt="Logo" className="h-120 w-auto ml-80" />
-             <h2 className='text-black font-semibold text-5xl ml-60 mt-122'>IEDC ASIET</h2>
+            <div className="ml-60 mt-122">
+            <h2 className='text-black font-semibold text-5xl mt-84'>IEDC ASIET</h2>
+            <p className='text-black font-medium text-xl mt-10 mr-8' style={{fontFamily: 'url("src/assets/Fonts/Ubuntu-Regular.ttf")'}}>ASIET, ideally located in an idyllic ambience, the Institution kindles vibrant memories of the serene presence of Jagadguru Adi Shankara. ASIET runs by Adisankara Trust, under the aegis of Sringeri mutt ,has a legacy of establishing renowned educational institutions for last 50 years.<br></br><br></br>
+            Affiliated to APJ Abdul Kalam Technological University, Kerala and approved by AICTE, New Delhi. ASIET is one among the premium institute in the state which offers Nation Board of Accreditation (NBA) programs in CSE, ECE, ME & EEE. ASIET provides opportunities of Innovation, Incubation, and Entrepreneurship for its students and public. ASIET hosted an exciting student venture named as Adi Shankara IEDC Boot Camp in 2015 with the support of Kerala Startup Mission (Technopark TBI). This consist of a facility setup in 15000sqft which includes IEDC, FAB Lab and Business Incubation Centre. In order to enhance the innovative eco system ASIET is right now focusing to setup a social incubation centre in our campus Among the 216 IEDC’s in the state, Kerala Startup Mission has selected Adi Shankara IEDC Bootcamp as the best IEDC in the state for the past 6 years ASIET FABLAB, State of Art laboratory for digital fabrication facility is equipped to create 'almost anything'. This lab is meant for the faculties as well as for the student to create any creative design as projects. This facility is open for all disciplines in technology as well as outside partners with permission.  Evaluating our IEDC performance Kerala State Industrial Development Cooperation have sanctioned Business Incubation Centre at Adishankara. Currently 12 of our alumni startups got incubated in the same. Our IEDC have also received the Entrepreneurship Enabler Award 2018 from KSUM considering the exemplary works done over the past years.</p>
+        </div>
         </div>
     </section>
   );
--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Events = () => {
   return (
-    <section className="bg-white-500 text-black h-screen flex flex-col justify-center mt-0">
+    <section id='our-events' className="bg-white-500 text-black h-screen flex flex-col justify-center -mt-80">
         <div className="text-center mb-4">
             <h1 className="text-7xl font-medium inline">Our</h1>&nbsp;&nbsp;&nbsp;&nbsp;
             <h1 className="text-7xl font-semibold inline">Events</h1>
