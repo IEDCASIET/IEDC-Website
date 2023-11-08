96fe2059e1ed7701b3596083f4c1b81fc3e5bfa9
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-11-09 02:50:24 +0530

    created navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,22 +8,58 @@ export default function Navbar() {
                 <img src={logo} alt="Logo" className="h-20 w-auto" />
                 <ul className="flex space-x-20 text-2xl">
                     <li className="text-black relative">
-                        <span className="hover:bg-gradient-to-r hover:from-pink-500 hover:via-white-500 hover:to-blue-500 px-5 py-3 rounded-full hover:text-white">About Us</span>
+                        <span
+                            className="hover:bg-gradient-to-r hover:from-pink-500 hover:via-white-500 hover:to-blue-500 px-5 py-3 rounded-full hover:text-white"
+                            onClick={() => {
+                                const aboutUsSection = document.getElementById('about-us');
+                                aboutUsSection.scrollIntoView({ behavior: 'smooth' });
+                            }}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            About Us
+                        </span>
                         <div className="hidden bg-gray-300 absolute top-full left-0 p-2 rounded-lg">
                         </div>
                     </li>
                     <li className="text-black relative">
-                        <span className="hover:bg-gradient-to-r hover:from-pink-500 hover:via-white-500 hover:to-blue-500 px-5 py-3 rounded-full hover:text-white">Events</span>
+                        <span
+                            className="hover:bg-gradient-to-r hover:from-pink-500 hover:via-white-500 hover:to-blue-500 px-5 py-3 rounded-full hover:text-white"
+                            onClick={() => {
+                                const aboutUsSection = document.getElementById('our-events');
+                                aboutUsSection.scrollIntoView({ behavior: 'smooth' });
+                            }}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            Events
+                        </span>
                         <div className="hidden bg-gray-300 absolute top-full left-0 p-2 rounded-lg">
                         </div>
                     </li>
                     <li className="text-black relative">
-                        <span className="hover:bg-gradient-to-r hover:from-pink-500 hover:via-white-500 hover:to-blue-500 px-5 py-3 rounded-full hover:text-white">Excom</span>
+                        <span
+                            className="hover:bg-gradient-to-r hover:from-pink-500 hover:via-white-500 hover:to-blue-500 px-5 py-3 rounded-full hover:text-white"
+                            onClick={() => {
+                                const aboutUsSection = document.getElementById('our-execom');
+                                aboutUsSection.scrollIntoView({ behavior: 'smooth' });
+                            }}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            Execom
+                        </span>
                         <div className="hidden bg-gray-300 absolute top-full left-0 p-2 rounded-lg">
                         </div>
                     </li>
                     <li className="text-black relative">
-                        <span className="hover:bg-gradient-to-r hover:from-pink-500 hover:via-white-500 hover:to-blue-500 px-5 py-3 rounded-full hover:text-white">Contact Us</span>
+                        <span
+                            className="hover:bg-gradient-to-r hover:from-pink-500 hover:via-white-500 hover:to-blue-500 px-5 py-3 rounded-full hover:text-white"
+                            onClick={() => {
+                                const aboutUsSection = document.getElementById('contact-us');
+                                aboutUsSection.scrollIntoView({ behavior: 'smooth' });
+                            }}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            Contact Us
+                        </span>
                         <div className="hidden bg-gray-300 absolute top-full left-0 p-2 rounded-lg">
                         </div>
                     </li>
