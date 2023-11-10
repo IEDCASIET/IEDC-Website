6bc94a9eac88637f5f5d2c7613f827bdf1e32f8a
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-11-10 20:50:54 +0530

    created card label

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import man from '../../assets/man.png'
+import hand from '../../assets/hand.png'
+import bulb from '../../assets/bulb.png'
+import cup from '../../assets/cup.png'
+
+
+const Card = () => {
+  return (
+    <div className="bg-blue-400 w-auto h-40 py-4 bg-gradient-to-r from-pink-500 to-blue-500 text-white text-xl flex items-center">
+    <img src={cup} alt='cup' className='h-24 ml-[12.5rem]' />
+    <div className="ml-2">
+        <p className='text-6xl text-white mb-2' style={{ fontFamily: 'Arial, sans-serif' }}>10+</p>
+        <p className='text-3xl text-white ml-1 font-semibold' style={{ fontFamily: 'Arial, sans-serif' }}>Years</p>
+    </div>
+    <img src={man} alt='man' className='h-24 ml-[12.5rem]' />
+    <div className="ml-2">
+        <p className='text-6xl text-white mb-2' style={{ fontFamily: 'Arial, sans-serif' }}>2000+</p>
+        <p className='text-3xl text-white ml-1 font-semibold' style={{ fontFamily: 'Arial, sans-serif' }}>Members</p>
+    </div>
+    <img src={bulb} alt='bulb' className='h-24 ml-[12.5rem]' />
+    <div className="ml-4">
+        <p className='text-6xl text-white mb-2' style={{ fontFamily: 'Arial, sans-serif' }}>30+</p>
+        <p className='text-3xl text-white ml-1 font-semibold' style={{ fontFamily: 'Arial, sans-serif' }}>Projects</p>
+    </div>
+    <img src={hand} alt='hand' className='h-24 ml-[12.5rem]' />
+    <div className="ml-2">
+        <p className='text-6xl text-white mb-2' style={{ fontFamily: 'Arial, sans-serif' }}>15+</p>
+        <p className='text-3xl text-white ml-1 font-semibold' style={{ fontFamily: 'Arial, sans-serif' }}>Start-Ups</p>
+    </div>
+</div>
+
+
+  );
+}
+
+export default Card;
