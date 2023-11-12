a7de5734efc43d8434c143065f78c168f96cc879
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-11-12 20:54:17 +0530

    installed swiper react-slick packages and made configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,15 @@ module.exports = {
     extend: {
       spacing: {
         '84': '21rem',
+        '97': '25rem',
         '120':'30rem',
         '121':'55rem',
         '122':'-15rem',
-        '123':'40rem'
+        '123':'40rem',
+        '124':'36rem'
       }
     },
   },
   plugins: [],
 }
+
