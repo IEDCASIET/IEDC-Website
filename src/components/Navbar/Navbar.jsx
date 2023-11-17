7662161f8f1d4cb38414e736d4937b87ceb3cd93
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-11-17 19:46:49 +0530

    made navbar sticky and bg blur

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/IEDC ASIET Logo.png";
 
 export default function Navbar() {
     return (
-        <nav className="p-4 w-full relative top-0 z-10">
+        <nav className="p-4 w-full sticky top-0 z-10 backdrop-blur">
             <div className="container mx-auto flex justify-between items-center">
                 <img src={logo} alt="Logo" className="h-20 w-auto" />
                 <ul className="flex space-x-20 text-2xl">
@@ -18,50 +18,42 @@ export default function Navbar() {
                         >
                             About Us
                         </span>
-                        <div className="hidden bg-gray-300 absolute top-full left-0 p-2 rounded-lg">
-                        </div>
                     </li>
                     <li className="text-black relative">
                         <span
                             className="hover:bg-gradient-to-r hover:from-pink-500 hover:via-white-500 hover:to-blue-500 px-5 py-3 rounded-full hover:text-white"
                             onClick={() => {
-                                const aboutUsSection = document.getElementById('our-events');
-                                aboutUsSection.scrollIntoView({ behavior: 'smooth' });
+                                const eventsSection = document.getElementById('our-events');
+                                eventsSection.scrollIntoView({ behavior: 'smooth' });
                             }}
                             style={{ cursor: 'pointer' }}
                         >
                             Events
                         </span>
-                        <div className="hidden bg-gray-300 absolute top-full left-0 p-2 rounded-lg">
-                        </div>
                     </li>
-                    <li className="text-black relative">
+                    <li className="text-black relative top-0">
                         <span
                             className="hover:bg-gradient-to-r hover:from-pink-500 hover:via-white-500 hover:to-blue-500 px-5 py-3 rounded-full hover:text-white"
                             onClick={() => {
-                                const aboutUsSection = document.getElementById('our-execom');
-                                aboutUsSection.scrollIntoView({ behavior: 'smooth' });
+                                const execomSection = document.getElementById('our-execom');
+                                execomSection.scrollIntoView({ behavior: 'smooth' });
                             }}
                             style={{ cursor: 'pointer' }}
                         >
                             Execom
                         </span>
-                        <div className="hidden bg-gray-300 absolute top-full left-0 p-2 rounded-lg">
-                        </div>
                     </li>
                     <li className="text-black relative">
                         <span
                             className="hover:bg-gradient-to-r hover:from-pink-500 hover:via-white-500 hover:to-blue-500 px-5 py-3 rounded-full hover:text-white"
                             onClick={() => {
-                                const aboutUsSection = document.getElementById('contact-us');
-                                aboutUsSection.scrollIntoView({ behavior: 'smooth' });
+                                const contactSection = document.getElementById('contact-us');
+                                contactSection.scrollIntoView({ behavior: 'smooth' });
                             }}
                             style={{ cursor: 'pointer' }}
                         >
                             Contact Us
                         </span>
-                        <div className="hidden bg-gray-300 absolute top-full left-0 p-2 rounded-lg">
-                        </div>
                     </li>
                 </ul>
             </div>
