6dc881a2a9e89e94742e698008cbf0bd59420c54
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-12-08 18:14:07 +0530

    made some changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,24 @@
 import { Element } from 'react-scroll';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
-import Events from './pages/Events/Events';
+import Events from './components/Events/Events';
 import Excom from './pages/Excom/Excom';
 import Contact from './pages/Contact/Contact';
 import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar'; 
+import CompanyCard from './components/Company/Company';
 
 export default function App() {
   return (
     <div>
-      <Navbar/>
-       <Element name="Home">
-        <Home />
-      </Element>
-
-      <Element name="About">
-        <About />
-      </Element>
-
-      <Element name="Events">
-        <Events />
-      </Element>
-
-      <Element name="Excom">
-        <Excom />
-      </Element>
-
-      <Element name="Contact">
-        <Contact />
-      </Element>
-
-      <Footer />
+      {/* <Navbar /> */}
+       {/* <Home /> */}
+       <About />
+       {/* <CompanyCard /> */}
+       <Events />
+       <Excom />
+       {/* <Contact /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
