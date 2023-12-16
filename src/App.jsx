ea17b9ef904d99fa1402e835f0d1181fd47bea8c
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-12-16 13:21:07 +0530

    added cards

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Contact from './pages/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar'; 
 import CompanyCard from './components/Company/Company';
+import Card from './components/Card/Card';
+import FooterCard from './components/Footer/FooterCard';
 
 export default function App() {
   return (
@@ -14,10 +16,12 @@ export default function App() {
       <Navbar />
        <Home />
        <About />
-       {/* <CompanyCard /> */}
+       <CompanyCard />
        <Events />
        <Excom />
+       <Card />
        <Contact />
+       <FooterCard />
       <Footer />
     </div>
   );
